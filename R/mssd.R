4728c42ae834b9ac33b4c002e62806ea74d9c7a2
Thomas Wieland <thomas.wieland.geo@googlemail.com>
2018-10-13 10:40:03 +0000

    version 2.0.3

---
+++ R/mssd.R
@@ -0,0 +1,23 @@
+mssd <- 
+  function (x) {
+  
+  samplesize <- length(x)
+  
+  i <- 0
+  j <- 1
+  
+  diffscores <- vector()
+  
+  for (i in 2:samplesize)
+  {
+    diffscores[j] <- x[i]-x[i-1]
+    j <- j+1
+  }
+  
+  diffscores_sq <- diffscores^2
+ 
+  mssd_val <- sum(diffscores_sq)/(samplesize-1)
+  
+  return(mssd_val)
+  
+}
